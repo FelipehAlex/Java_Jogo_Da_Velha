d231ffd797f0b55665cff593f616dc1206c8a4d7
Felipe Alex <81978216+FelipehAlex@users.noreply.github.com>
2021-06-06 16:26:15 -0300

    Jogo da velha

--- program/Program.java
+++ program/Program.java
@@ -2,7 +2,7 @@ package program;
 
 
 import java.util.Scanner;
-import teste.ChecarVencedor;
+import teste.ChecarTabela;
 
 public class Program {
 
@@ -25,21 +25,21 @@ public class Program {
                 System.out.print("Digite uma posição entre '0' e 8: ");
                 jogador_numero_1 = sc.nextInt();
 
-                if (ChecarVencedor.verificaTabela(tabela, jogador_numero_1)) {
+                if (ChecarTabela.verificaTabela(tabela, jogador_numero_1)) {
                     tabela[jogador_numero_1] = j1;
                     jogador = 2;
-                    System.out.print(ChecarVencedor.desenhoTabela(tabela));
+                    System.out.print(ChecarTabela.desenhoTabela(tabela));
                 } else {
-                    System.out.println(ChecarVencedor.desenhoTabela(tabela));
+                    System.out.println(ChecarTabela.desenhoTabela(tabela));
                     System.out.println("Posição ja ocupada");
                 }
             }
 
-            if (ChecarVencedor.verificaVitoria(tabela, j1)) {
+            if (ChecarTabela.verificaVitoria(tabela, j1)) {
                 System.out.print("Jogador numero 1 ganhou");
                 break;
             }
-            if (ChecarVencedor.verificaEmpate(tabela)) {
+            if (ChecarTabela.verificaEmpate(tabela)) {
                 System.out.print("Empate");
                 break;
             }
@@ -49,21 +49,21 @@ public class Program {
                 System.out.print("Digite uma posição entre '0' e 8: ");
                 jogador_numero_2 = sc.nextInt();
                 
-                if(ChecarVencedor.verificaTabela(tabela, jogador_numero_2)){
+                if(ChecarTabela.verificaTabela(tabela, jogador_numero_2)){
                     tabela[jogador_numero_2] = j2;
                     jogador = 1;
-                    System.out.println(ChecarVencedor.desenhoTabela(tabela));
+                    System.out.println(ChecarTabela.desenhoTabela(tabela));
                 }
                 else{
-                    System.out.println(ChecarVencedor.desenhoTabela(tabela));
+                    System.out.println(ChecarTabela.desenhoTabela(tabela));
                     System.out.println("Posição já ocupada");
                 }
             }
-            if (ChecarVencedor.verificaVitoria(tabela, j2)){
+            if (ChecarTabela.verificaVitoria(tabela, j2)){
                 System.out.println("Jogador numero 2 ganhou");
                 break;
             }
-            if (ChecarVencedor.verificaEmpate(tabela)){
+            if (ChecarTabela.verificaEmpate(tabela)){
                 System.out.println("Empate");
                 break;
             }
---
+++ teste/ChecarTabela.java
@@ -0,0 +1,78 @@
+package teste;
+
+public abstract class ChecarTabela {
+    
+    public static String desenhoTabela(String[] desenho){
+        String jogoDaVelha = "";
+        for (int i=0; i<desenho.length; i++){
+            if (i == 2 || i == 5 || i == 8){
+                jogoDaVelha += " " + desenho[i] + " \n";
+            }else{
+                jogoDaVelha += " " + desenho[i] + " |";
+            }
+        }
+        return jogoDaVelha;
+    }
+    
+    
+    public static boolean verificaVitoria(String[] vect, String jogador){
+        boolean resultado = false;
+        
+        for (int i=0; i<vect.length; i++){
+            
+            //linha
+            if (vect[0].equals(jogador) && vect[1].equals(jogador) && vect[2].equals(jogador)){
+                resultado = true;
+            }
+            else if (vect[3].equals(jogador) && vect[4].equals(jogador) && vect[5].equals(jogador)){
+                resultado = true;
+            }
+            else if(vect[6].equals(jogador) && vect[7].equals(jogador) && vect[8].equals(jogador)){
+                resultado = true;
+            }
+            
+            //coluna
+            else if(vect[0].equals(jogador) && vect[3].equals(jogador) && vect[6].equals(jogador)){
+                resultado = true;
+            }
+            else if(vect[1].equals(jogador) && vect[4].equals(jogador) && vect[7].equals(jogador)){
+                resultado = true;
+            }
+            else if(vect[2].equals(jogador) && vect[5].equals(jogador) && vect[8].equals(jogador)){
+                resultado = true;
+            }
+            
+            //vertical
+            else if(vect[0].equals(jogador) && vect[4].equals(jogador) && vect[8].equals(jogador)){
+                resultado = true;
+            }
+            else if(vect[6].equals(jogador) && vect[4].equals(jogador) && vect[2].equals(jogador)){
+                resultado = true;
+            }
+            
+        }
+        return resultado;
+    }
+    
+    public static boolean verificaTabela(String[] vect, int posicao){
+        boolean resultado = false;
+        
+        for(int i=0; i<vect.length; i++){
+            if (vect[posicao].equals("_")){
+                resultado = true;
+            }
+        }  
+        return resultado;   
+    }
+    
+    public static boolean verificaEmpate(String[] vect){
+        boolean resultado = true;
+        for (int i=0; i<vect.length; i++){
+            if (vect[i].equals("_")){
+                resultado = false;
+            }
+        }  
+        return resultado;
+    }
+    
+}
